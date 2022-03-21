647d19864a82d35b577901899273178672c221b9
luo-yifan <yifanluo@umich.edu>
2022-03-21 16:53:44 -0700

    - add lines and well types - remove redundant colnames and xlsx to csv

--- app.r
+++ app.r
@@ -10,9 +10,10 @@ source("server.R")
 
 ## run app
 shinyApp(ui = ui, server = server)
-
+# 
 # library(rsconnect)
 # rsconnect::setAccountInfo(name='yifanluo',
 #                           token='801F6411CB4C8EFD33298155AE6A8725',
 #                           secret='0B0CMhurDaBk0BtwqUEx+VxVguLFT+9yt4VYzwx2')
+
 # rsconnect::deployApp()
